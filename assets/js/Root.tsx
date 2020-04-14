6828d50d439b0f1a8209c27453f0db8499c1d13c
koppe_pan <takas3141@gmail.com>
2020-04-14 19:19:58 +0900

    react counter demo

--- assets/js/Root.tsx
+++ assets/js/Root.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Header from './components/Header'
 import HomePage from './pages'
+import Counter from './components/Counter'
 
 export default class Root extends React.Component {
   public render(): JSX.Element {
@@ -12,6 +13,7 @@ export default class Root extends React.Component {
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={HomePage} />
+            <Route path="/counter" component={ Counter } />
           </Switch>
         </BrowserRouter>
       </>
